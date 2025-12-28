1ab2e886d0bf8c778d3ad61d08e3b02b76240da0
talalayazkul-ai <talalayazkul@gmail.com>
2025-12-28 03:46:22 +0100

    Time: 0 ms (100.00%) | Memory: 12.4 MB (84.86%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -8,8 +8,8 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
         i = m - 1
-        j = n - 1
-        k = m + n - 1
+        j = n -1
+        k = m + n -1
 
         while i >= 0 and j >= 0:
             if nums1[i] > nums2[j]:
@@ -19,9 +19,9 @@ class Solution(object):
                 nums1[k] = nums2[j]
                 j -= 1
             k -= 1
-
         
         while j >= 0:
             nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
+            j -=1
+            k -=1
+        return nums1
