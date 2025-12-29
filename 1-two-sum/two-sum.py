e0c1b0c6d46a40237e1d8e7e9e1d1a421bc346ef
talalayazkul-ai <talalayazkul@gmail.com>
2025-12-29 23:03:59 +0100

    Time: 0 ms (100.00%) | Memory: 13.2 MB (60.02%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,13 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-    
         seen = {}
 
         for i in range(len(nums)):
             need = target - nums[i]
-
+            
             if need in seen:
-                return [seen[need],i]
+                return [seen[need], i]
 
             seen[nums[i]] = i
+
