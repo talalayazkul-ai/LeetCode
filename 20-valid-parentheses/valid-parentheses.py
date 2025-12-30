2a05428c4a6b3a3bc37afda0fb42513e7bbe5976
talalayazkul-ai <talalayazkul@gmail.com>
2025-12-30 01:54:57 +0100

    Time: 0 ms (100.00%) | Memory: 12.7 MB (9.78%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        pairs = { ')': '(' , ']': '[' , '}':'{'}
+
+        for ch in s:
+            if ch in pairs:
+                if stack and stack[-1] == pairs[ch]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(ch)
+
+        return True if not stack else False
